dc12525299c89cdf10e44ac0c6d104ad7ecb9435
Gregory Hawman <g.hawman@gmail.com>
2020-02-14 09:26:19 -0800

    finished tabs compontent. it creats a new tab, and grabs the content using axios and the given api.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,22 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        response.data.topics.forEach(element => {
+            entryPoint.appendChild(createTabs(element));
+        })
+});
+
+
+
+const entryPoint = document.querySelector('.topics');
+
+function createTabs(obj) {
+    const newTab = document.createElement('div');
+    newTab.classList.add('tab');
+    newTab.textContent = obj;
+    return newTab;
+};
+
